501d5439362f2f7cf6eae086fd68da743f87d73d
Jacobo Guillen 2006 <jacoboguillen79@gmail.com>
2022-03-30 17:24:01 -0500

    se guarda el proyecto

--- controlador.js
+++ controlador.js
@@ -1,1 +1,22 @@
-let boton=document.getElementById
+let boton=document.getElementById ("boton") 
+    boton.addEventListener ("click",function(event){
+
+        alert("hiciste click")
+
+
+        let selector=document.getElementById("selector").value
+        alert("ustred eligio el"+selector)
+        switch(selector){
+            case "1":
+                alert("elgiste la sur-norte")
+                break 
+            case "2":
+                alert("elgiste la oriental")
+                break 
+            case "3":
+                 alert("elgiste la occidental")
+                 break   
+        }  
+        
+        
+    } )
